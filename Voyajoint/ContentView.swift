d01f8d3236d555a13859006e0556dfaf5dd2e950
“pickle-debug” <1941109171@qq.com>
2024-03-18 17:34:17 +0800

    first commit

--- Voyajoint/ContentView.swift
+++ Voyajoint/ContentView.swift
@@ -9,10 +9,21 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
+    // 为地图设置一个初始区域
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
+            
+            // 使用 Map 视图展示地图
+            Map(coordinateRegion: $region)
+                .edgesIgnoringSafeArea(.all) // 如果你想地图忽略安全区，可加此行
+            
             Text("Hello, world!")
         }
         .padding()
