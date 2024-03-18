a8f3919ff6b302fd1565314c4f73360b5608f648
“pickle-debug” <1941109171@qq.com>
2024-03-18 10:30:46 +0800

    Initial Commit

---
+++ Voyajoint/VoyajointApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoyajointApp.swift
+//  Voyajoint
+//
+//  Created by Tanshuo on 2024/3/18.
+//
+
+import SwiftUI
+
+@main
+struct VoyajointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
